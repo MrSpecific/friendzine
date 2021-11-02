1cde7f8f0675c9a64e25b18d36210ac9d63af8e4
Will Christenson <will@wlcr.io>
2021-11-01 22:54:08 -0700

    Data

---
+++ data/datocms.js
@@ -0,0 +1,60 @@
+import { gql, GraphQLClient } from 'graphql-request';
+
+export { gql };
+
+export function request({ query, variables, preview }) {
+  const endpoint = preview ? `https://graphql.datocms.com/preview` : `https://graphql.datocms.com/`;
+
+  const client = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
+    },
+  });
+
+  return client.request(query, variables);
+}
+
+// See: https://www.datocms.com/blog/offer-responsive-progressive-lqip-images-in-2020
+export const responsiveImageFragment = gql`
+  fragment responsiveImageFragment on ResponsiveImage {
+    srcSet
+    webpSrcSet
+    sizes
+    src
+    width
+    height
+    aspectRatio
+    alt
+    title
+    bgColor
+    base64
+  }
+`;
+
+const RECIPE_LIST_QUERY = gql`
+  query HomePage($limit: IntType) {
+    allRecipes(first: $limit) {
+      title
+      slug
+      cover {
+        responsiveImage(imgixParams: { fit: crop, w: 600, h: 600 }) {
+          ...responsiveImageFragment
+        }
+      }
+    }
+  }
+
+  ${responsiveImageFragment}
+`;
+
+export async function getRecipeList(options = {}) {
+  const { limit = 10 } = options;
+
+  const data = await request({
+    query: RECIPE_LIST_QUERY,
+    variables: { limit },
+    preview: false,
+  });
+
+  return data.allRecipes;
+}
---
+++ pages/issue/[issueSlug].js
@@ -0,0 +1,83 @@
+import { useRouter } from 'next/router';
+import { gql, getPaths, runQuery } from '@data/datocms';
+
+import Header from '../../components/Header';
+
+export default function IssuePage(props) {
+  const { fields } = props;
+  const router = useRouter();
+  const id = router.query;
+
+  return (
+    <>
+      <Header>Issue</Header>
+      <h2>{fields.title}</h2>
+    </>
+  );
+}
+
+const ISSUE_SLUGS_QUERY = gql`
+  query IssueSlugs() {
+    issue() {
+      slug
+    }
+  }
+`;
+
+export async function getStaticPaths() {
+  const data = await request({
+    query: ISSUE_SLUGS_QUERY,
+    variables: { limit },
+    preview: false,
+  });
+
+  const paths = getPaths(allRecipes, 'recipeSlug');
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+const SINGLE_ISSUE_QUERY = gql`
+  query Issue($slug: String) {
+    recipe(filter: { slug: { eq: $slug } }) {
+      title
+      date
+      # description
+      slug
+      # cover {
+      #   responsiveImage(imgixParams: { fit: crop, w: 1400, h: 800 }) {
+      #     ...responsiveImageFragment
+      #   }
+      # }
+      # author {
+      #   name
+      # }
+      # categories {
+      #   title
+      #   slug
+      # }
+      # tags {
+      #   title
+      # }
+      # gallery {
+      #   id
+      # }
+    }
+  }
+
+  ${responsiveImageFragment}
+`;
+
+export async function getStaticProps({ params }) {
+  const data = await request({
+    query: SINGLE_ISSUE_QUERY,
+    variables: { slug: params.recipeSlug },
+    preview: false,
+  });
+
+  return {
+    props: data.recipe,
+  };
+}
