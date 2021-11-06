70811812deed50bfe45a8217b284ae3208d2c32a
Will Christenson <will@wlcr.io>
2021-11-06 12:09:22 -0700

    Locale string

--- components/Attribution.js
+++ components/Attribution.js
@@ -1,11 +1,13 @@
 import styles from '@styles/components/Attribution.module.css';
 
 const Attribution = ({ date }) => {
+  const dateObj = date && new Date(date);
   return (
     <div className="attribution">
       {date && (
         <time dateTime={date} pubdate="pubdate" className={styles.date}>
-          Published on {date}
+          Published on{' '}
+          {dateObj.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
         </time>
       )}
     </div>
