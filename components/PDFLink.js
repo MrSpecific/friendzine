ef3b1e318022c03312795f8007a863c9ea5c5588
Will Christenson <will@wlcr.io>
2021-11-06 12:56:27 -0700

    PDF link work

--- components/PDFLink.js
+++ components/PDFLink.js
@@ -4,7 +4,7 @@ import styles from '@styles/components/PDFLink.module.css';
 const PDFLink = ({ pdf }) => {
   if (!pdf) return null;
   return (
-    <a href={pdf.url} className={styles.pdfLink}>
+    <a href={pdf.url} className={styles.pdfLink} target="_blank" rel="noreferrer">
       <PDFIcon /> Download this issue
     </a>
   );
