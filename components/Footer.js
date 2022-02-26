e8b0ef38a22608a1a17a3eec3de1a484a48d420a
Will Christenson <will@wlcr.io>
2022-02-25 23:14:26 -0800

    Add attribution

--- components/Footer.js
+++ components/Footer.js
@@ -5,14 +5,19 @@ import TwitterLogo from './svg/twitter.svg';
 const Footer = (props) => {
   return (
     <footer className={styles.footer}>
-      <a href="//instagram.com/frndzine" target="_blank" rel="noopener noreferrer">
-        <InstagramLogo />
-        <span className="visually-hidden">Instagram</span>
-      </a>
-      <a href="//twitter.com/frndzine" target="_blank" rel="noopener noreferrer">
-        <TwitterLogo />
-        <span className="visually-hidden">Twitter</span>
-      </a>
+      <div className={styles.attribution}>
+        &copy; 2021-2022 Trevor Peterson &amp; Will Christenson
+      </div>
+      <div className={styles.footerSocials}>
+        <a href="//instagram.com/frndzine" target="_blank" rel="noopener noreferrer">
+          <InstagramLogo />
+          <span className="visually-hidden">Instagram</span>
+        </a>
+        <a href="//twitter.com/frndzine" target="_blank" rel="noopener noreferrer">
+          <TwitterLogo />
+          <span className="visually-hidden">Twitter</span>
+        </a>
+      </div>
     </footer>
   );
 };
